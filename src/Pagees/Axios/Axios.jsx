672ce770f275802723d66c5a4fb95a1fae2655d1
sahanpramudith <pramuditsahan466@gmail.com>
2024-01-24 15:15:41 +0530

    get-axios post-axios map-axios-getdata

--- src/Pagees/Axios/Axios.jsx
+++ src/Pagees/Axios/Axios.jsx
@@ -1,17 +1,21 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import {Button} from "@mui/material";
+import {Box} from '@mui/material';
+import PostCard from "../../Component/Post Card/PostCard.jsx";
 
 export default function Axios() {
     useEffect(() => {
         GetAxios()
     }, []);
+    const [data, setData] = useState([])
 
     const GetAxios = () => {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(function (response) {
                 // handle success
-                console.log(response);
+                // console.log(response);
+                setData(response.data);
             })
             .catch(function (error) {
                 // handle error
@@ -21,7 +25,7 @@ export default function Axios() {
                 // always executed
             });
     }
-    
+
     const Post = () => {
         axios.post('https://jsonplaceholder.typicode.com/posts', {
             title: 'foo',
@@ -35,14 +39,47 @@ export default function Axios() {
                 console.log(error);
             });
     }
-    
-    return(
+
+    const UpDate = () => {
+        axios.put('https://jsonplaceholder.typicode.com/posts/1', {
+            id: 1,
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+        })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }
+
+    return (
         <div>
             <h1>
                 Axios
             </h1>
             <br/>
-            <Button onClick={()=>Post()} variant="contained">Contained</Button>
+            <Button onClick={() => Post()} variant="contained">Contained</Button>
+            <br/>
+            <hr/>
+            <h1>Up daate </h1>
+            <Button onClick={() => UpDate()} variant="contained">Up Date </Button>
+            <hr/>
+            <h1></h1>
+            <h1>Delet </h1>
+            <Button onClick={() => UpDate()} variant="contained">Up Date </Button>
+            <br/>
+            <hr/>
+            <hr/>
+            <hr/>
+            <Box>
+                {data.map((val,index)=>(
+                    <PostCard id={val.id} description={val.body} tital={val.tital} />
+                ))}
+
+            </Box>
         </div>
     )
 }
